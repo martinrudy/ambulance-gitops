a94b523ff92771e48f2cfd3ea4b6755712d8a260
Martin  Rudolf <rudolfmato@gmail.com>
2024-02-25 12:37:57 +0100

    update

---
+++ mrud-ambulance-ufe/stencil.config.ts
@@ -0,0 +1,25 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'ambulance-ufe',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+  globalScript: 'src/global/app.ts'
+};
